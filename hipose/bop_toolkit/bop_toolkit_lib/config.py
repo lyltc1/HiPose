d33f012835dc446b50895e48ed76e23bcf421dcb
lyltc1 <870767645@qq.com>
2024-04-01 20:06:32 +0800

    first commit

--- hipose/bop_toolkit/bop_toolkit_lib/config.py
+++ hipose/bop_toolkit/bop_toolkit_lib/config.py
@@ -12,7 +12,7 @@ import os
 if 'BOP_PATH' in os.environ:
   datasets_path = os.environ['BOP_PATH']
 else:
-  datasets_path = r'/home/z3d/data'
+  datasets_path = r'/home/HiPose/data'
 
 # Folder with pose results to be evaluated.
 results_path = r'/path/to/folder/with/results'
