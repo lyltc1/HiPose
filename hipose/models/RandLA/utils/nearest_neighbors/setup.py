54c39fde50d2383cbed83f5ffad51afeda6eb47f
LiTang <litang0617@outlook.com>
2025-05-20 16:33:22 +0800

    update RandLA for higher version of pytorch

--- hipose/models/RandLA/utils/nearest_neighbors/setup.py
+++ hipose/models/RandLA/utils/nearest_neighbors/setup.py
@@ -1,21 +1,21 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from setuptools import setup, Extension
 import numpy
+import os
 
-
+# 确保当前工作目录是setup.py所在的目录
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 ext_modules = [Extension(
        "nearest_neighbors",
-       sources=["knn.pyx", "knn_.cxx",],  # source file(s)
+       sources=["knn_.cxx"],
        include_dirs=["./", numpy.get_include()],
        language="c++",            
-       extra_compile_args = [ "-std=c++11", "-fopenmp",],
+       extra_compile_args = ["-std=c++11", "-fopenmp", "-DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION"],
        extra_link_args=["-std=c++11", '-fopenmp'],
+       define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
   )]
 
 setup(
-    name = "KNN NanoFLANN",
-    ext_modules = ext_modules,
-    cmdclass = {'build_ext': build_ext},
+    name="KNN_NanoFLANN",
+    ext_modules=ext_modules,
 )
