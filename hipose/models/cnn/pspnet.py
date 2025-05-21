6f975a5bbb09ac08ed7acf63a3d7177a8ad358a9
LiTang <litang0617@outlook.com>
2025-05-21 18:44:16 +0800

    update upsample api to interpolation api

--- hipose/models/cnn/pspnet.py
+++ hipose/models/cnn/pspnet.py
@@ -23,7 +23,7 @@ class PSPModule(nn.Module):
     def forward(self, feats):
         h, w = feats.size(2), feats.size(3)
         priors = [
-            F.upsample(input=stage(feats), size=(h, w), mode='bilinear')
+            F.interpolate(input=stage(feats), size=(h, w), mode='bilinear')
             for stage in self.stages
         ] + [feats]
         bottle = self.bottleneck(torch.cat(priors, 1))
